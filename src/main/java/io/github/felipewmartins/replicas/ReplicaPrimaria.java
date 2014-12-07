530bba6cd3300fca1017f0ce06bd865e506fad36
Felipe Martins <felipewmartins@gmail.com>
2014-12-07 10:35:00 -0300

    ajustes

--- src/main/java/io/github/felipewmartins/replicas/ReplicaPrimaria.java
+++ src/main/java/io/github/felipewmartins/replicas/ReplicaPrimaria.java
@@ -1,5 +1,13 @@
 package io.github.felipewmartins.replicas;
 
-public class ReplicaPrimaria {
+import io.github.felipewmartins.task.Task;
+
+public class ReplicaPrimaria extends Servidor{
+
+  @Override
+  public <T> T executeTask(Task<T> t) {
+    // TODO Auto-generated methods stub
+    return null;
+  }
 
 }
